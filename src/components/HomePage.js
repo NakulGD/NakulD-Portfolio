9829ac83fbbc35acde45ea51a4a7d78c50bb46a3
NakulGD <nakulgd@gmail.com>
2023-11-20 18:26:55 -0800

    ON Button implementation

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styling/HomePage.css';
 
 const HomePage = () => {
+    const [buttonText, setButtonText] = useState('OFF');
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setButtonText('ON');
+        }, 3400); // 3400ms = 3s (animation delay) + 0.4s (animation duration)
+
+        return () => clearTimeout(timer);
+    }, []);
+
+
+
+
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-                    <h1 className="fade-in-text">Hello, my name is...</h1>
+        <div className="grid-container">
+            <h1 className="welcome-element"> Glad you're here, my name is</h1>
+            <div className="on-element">
+                <div className="button">
+                    <span>{buttonText}</span>
                 </div>
             </div>
+            <h1 className="text-flicker-in-glow name-element">Nakul Dharan</h1>
             {/* Additional rows and columns as needed */}
         </div>
     );
