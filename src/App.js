03a6449418b84fc92195c026fa1dbbb73d75c295
NakulGD <nakulgd@gmail.com>
2023-11-27 21:18:38 -0800

    Working website implementation v1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Landing from './components/Landing';
+import { motion, AnimatePresence } from 'framer-motion';
 import './App.css';
 import MainPage from './components/MainPage';
 
+
 function App() {
   const [homePageVisible, setHomePageVisible] = useState(false);
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1100);
+  const [landingPageSkipped, setLandingPageSkipped] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsLargeScreen(window.innerWidth > 1100);
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Initial check
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (!isLargeScreen) {
+      setHomePageVisible(true);
+      setLandingPageSkipped(true);
+    } else if (landingPageSkipped) {
+      // If the landing page was skipped and the screen is now large, don't show the landing page
+      setHomePageVisible(true);
+    }
+  }, [isLargeScreen, landingPageSkipped]);
 
   return (
     <div className="App">
-      <Landing onLandingAnimationEnd={() => setHomePageVisible(true)} />
+      {isLargeScreen && !landingPageSkipped && <Landing onLandingAnimationEnd={() => setHomePageVisible(true)} />}
       {homePageVisible && <MainPage />}
     </div>
   );
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,13 +4,14 @@ import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faFile } from '@fortawesome/free-regular-svg-icons';
 import './styling/MainPage.css';
-import Resume from '../assets/Nakul-Dharan-Resume-2023_2024Winter.pdf';
+import Resume from '../assets/Nakul_Dharan-Resume-FE-2023_2024Winter.pdf';
 import ubcLogo from '../assets/ubc_image.png';
 import cmdPrompt from '../assets/terminal.png'
 import { motion, AnimatePresence } from 'framer-motion';
 import ClashLogo from '../assets/clash_course_logo.png'
 import OS161Logo from '../assets/os161_logo.png'
 import SearchLogo from '../assets/search_engine_logo.png'
+import HeatingReport from '../assets/Heating_System_Report.pdf'
 
 const ColoredLine = ({ color }) => (
     <hr
@@ -25,30 +26,42 @@ const ColoredLine = ({ color }) => (
 
 
 const ProjectCard = ({ project, isSelected, onClick }) => {
-    // Animation variants
-    const variants = {
-        hidden: { opacity: 0 },
-        visible: { opacity: 1 },
-    };
-
+    
     return (
-        <div className={`project-card ${isSelected ? 'selected' : ''}`} onClick={() => onClick(project.id)}>
-            <h2>{project.title}</h2>
-            <h5>{project.subtitle}</h5>
+        <motion.div 
+            layout
+            initial={{ borderRadius: 15 }}
+            onClick={() => onClick(project.id)}
+            className='myproject-card'
+            whileHover={{ scale: 1.1 }}
+        >
+            <motion.h2 layout='position'>{project.title}</motion.h2>
             {isSelected && (
-                <motion.div 
-                    className="project-details"
-                    initial="hidden"
-                    animate="visible"
-                    exit="hidden"
-                    variants={variants}
-                    transition={{ duration: 2 }} // Adjust duration as needed
+                <motion.div
+                    layout = 'position'
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    className='expanded-content'
                 >
                     <p>{project.description}</p>
-                    <a href={project.github} className="project-link-button">GitHub Repo</a>
+                    {project.github && (
+                        <a href={project.github} className="card-link-button">
+                        GitHub
+                        </a>
+                    )}
+                    {project.website && (
+                        <a href={project.website} className="card-link-button">
+                        Website
+                        </a>
+                    )}
+                    {project.report && (
+                        <a href={project.report} className="card-link-button">
+                        Full Report
+                        </a>
+                    )}
                 </motion.div>
             )}
-        </div>
+        </motion.div>
     );
 };
 
@@ -81,16 +94,16 @@ const MainPage = () => {
     }, []);
 
 
-
     const projects = [
         { id: 'project1', title: 'Clash Course', subtitle: 'A University Course Planner', logo: ClashLogo, fontClass: 'font-style-1', description: 'Full-stack web application assisting students in visualizing course schedules and catching potential conflicts. Developed using HTML, CSS, and JavaScript, the app allows students to add courses and times to a schedule, visually highlighting any time conflicts between courses.',
         website: 'http://clashcourse.online',
         github: 'https://github.com/NakulGD/worklist_helper'},
-        //{ id: 'project2', title: 'OS161', subtitle: 'Operating System Implementation', logo: OS161Logo, fontClass: 'font-style-2'},
-        //{ id: 'project3', title: 'Search Engine', subtitle: 'Autocompletion Capable Search', logo: SearchLogo, fontClass: 'font-style-3'},
-        //{ id: 'project4', title: 'Content Curation System', subtitle: 'Content Curation for Twitter', logo: ClashLogo, fontClass: 'font-style-1' },
-        //{ id: 'project5', title: '16-Bit RISC Machine', subtitle: 'Turing-Complete Computer', logo: ClashLogo, fontClass: 'font-style-1' },
-        //{ id: 'project6', title: 'Automatic Heating System', subtitle: 'Built from an NTC Thermistor', logo: ClashLogo, fontClass: 'font-style-1' },
+        { id: 'project2', title: 'OS161', subtitle: 'Operating System Implementation', description: 'Developed by Harvard University as a teaching tool, OS/161 is a C-based, multicore operating system. Initially, the system has missing/primitive implementations of core operating system components. It was up to me to program, rigorously debug, and optimize these components to end up with a stable, reliable, and quick operating system. (GitHub link coming soon)', fontClass: 'font-style-2', website: null, github: null},
+        { id: 'project3', title: 'Search Engine', subtitle: 'Autocompletion Capable Search', logo: SearchLogo, description: 'A fully functional Java-based search engine leveraging Naive Bayesian Analysis and Laplacian Smoothing to enhance accuracy and reliability of autocomplete suggestions based on what the user has inputted.', fontClass: 'font-style-3', github: 'https://github.com/NakulGD/Search-Engine.git'},
+        { id: 'project4', title: 'Curation System', subtitle: 'Content Curation for Twitter', logo: ClashLogo, description: 'Java-based content curation service that utilized the Twitter API to provide a streamlined solution for managing social media activity. The service allowed for the addition and removal of subscribers, resulting in a customizable timeline of their latest unread tweets.', fontClass: 'font-style-1', github: 'https://github.com/NakulGD/Social-Media-Curation.git'},
+        { id: 'project5', title: 'RISC Machine', subtitle: 'Turing-Complete Computer', logo: ClashLogo, description:'A functional 16-bit Turing-complete Reduced Instruction Set Computer (RISC) machine. This computational device was constructed of a Datapath, a Finite State Machine (FSM) controller, Random Access Memory (RAM), and register modules. The entire development process was centered around the use of SystemVerilog, implemented on a Field-Programmable Gate Array (FPGA) board.', fontClass: 'font-style-1' },
+        { id: 'project6', title: 'Automatic Heating System', subtitle: 'Built from an NTC Thermistor', logo: ClashLogo, description: 'Developed a working implementation of an automatic thermostat, intricately designed around an NTC Thermistor, calculating temperature with 0.1℃ precision across a -2-67℃ spectrum.It contained a dual-speed motor control circuit, utilizing PWM through a 555 timer chip, to adeptly modulate a the speed of a connected fan, ensuring optimal energy efficiency and implementing an automatic shut-off feature to conserve power. Additionally, the system possesses data logging functionalities, logging temperature data to an SD card via Arduino, and subsequently craft a graph, providing a clear and insightful visual representation of the data.', 
+        fontClass: 'font-style-1', report: HeatingReport },
         // Add more projects
     ];
 
@@ -194,13 +207,12 @@ const MainPage = () => {
             <div className='projects-section'>
                 {projects.map(project => (
                     <ProjectCard 
-                        key={project.id} 
-                        project={project} 
-                        isSelected={selectedId === project.id} 
-                        onClick={handleCardClick} 
+                        key={project.id}
+                        project={project}
+                        isSelected={selectedId === project.id}
+                        onClick={handleCardClick}
                     />
                 ))}
-
 
             </div>
         </div>
